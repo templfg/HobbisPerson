1dc748ac4a265cf5a004c300e15b6106bfb22a92
Артём <156431980+templfg@users.noreply.github.com>
2024-12-24 10:37:43 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HobbyLibrary;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Hobby> hobbies = new List<Hobby>();
+            hobbies.Add(new Hobby("Баскетбол", "Кидать мячик в корзину"));
+            hobbies.Add(new Hobby("Вязание", "Крестиком и шпицами"));
+            hobbies.Add(new Hobby("Футбол", "Пинать мячик ногой, иногда головой"));
+
+            Person person = new Person("Сущик Артём", "Юбилейная 20А", hobbies);
+
+            person.ShowAll();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HobbyLibrary/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HobbyLibrary
+{
+    public class Hobby
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string description;
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public Hobby(string n, string d)
+        {
+            name = n;
+            description = d;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"{name}, описание - {description}");
+        }
+    }
+
+    public class Person
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string address;
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        private List<Hobby> hobbeis = new List<Hobby>();
+        public List<Hobby> Hobbies
+        {
+            get { return hobbeis; }
+            set { hobbeis = value; }
+        }
+
+        public Person(string n, string a, List<Hobby> h)
+        {
+            name = n;
+            address = a;
+            hobbeis = h;
+        }
+
+        public void ShowAll()
+        {
+            Console.WriteLine($"{name} живет по адресу: {address}");
+            Console.WriteLine($"Хобби:");
+            for (int i = 0; i < hobbeis.Count; i++)
+            {
+                Console.Write($"{i + 1} ");
+                hobbeis[i].Show();
+            }
+        }
+    }
+}
